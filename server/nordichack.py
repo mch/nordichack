600e08f91143690819aeef83edfd01e140add3f7
Matt Hughes <matt.c.hughes@gmail.com>
2017-01-14 22:59:58 -0700

    Inital try at the web server side of things.

--- server/nordichack.py
+++ server/nordichack.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import zmq
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
@@ -25,3 +26,43 @@ def connect_db():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/v1/desiredspeed', methods=['GET', 'POST'])
+def desiredspeed():
+    if request.method == 'POST':
+        speed = None
+        try:
+            speed = float(request.data)
+            if speed < 0.0 or speed > 30.0:
+                speed = None
+        except ValueError:
+            speed = None
+
+        if speed is None:
+            return flask.Response(response = "Invalid speed requested",
+                                  status = 400,
+                                  headers = None,
+                                  mimetype = "text/plain")
+        
+        set_desired_speed(float(request.data))
+    else:
+        return "0.0"    
+
+def set_desired_speed(speed):
+
+    # load calibration params from database
+    slope = 3.424
+    offset = 18.558
+    dutycycle = int(speed * slope + offset)
+
+    if speed == 0.0:
+        dutycycle = 0
+    
+    # a "service" for handling sending zmq requests?
+    c = zmq.Context()
+    s = c.socket(zmq.REQ)
+    s.connect("tcp://localhost:5555")
+    s.send(b"SETDUTYCYCLE %d" % dutycycle)
+    msg = s.recv()
+    print("reply: '%s'" % (msg, ))
+
