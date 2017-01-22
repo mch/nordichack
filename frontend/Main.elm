98f6b3267c311f818bb616cf3abc7c110346d9ea
Matt Hughes <matt.hughes@calgaryscientific.com>
2017-01-21 18:35:04 -0700

    Really nasty formatting code.

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -20,12 +20,15 @@ main = Html.program { init = init
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Time.every (Time.second * 0.25) Tick
 
 
 type alias Model =
     { speed : Int
     , requestedSpeed : Int
+    , startTime : Float
+    , currentTime : Float
+    , distance : Float
     , error : String
     }
 
@@ -34,10 +37,16 @@ type Msg = IncreaseSpeed
          | DecreaseSpeed
          | SetSpeed Int
          | SetSpeedResponse (Result Http.Error String)
+         | Tick Time.Time
 
 
 init : (Model, Cmd Msg)
-init = ({ speed = 0, requestedSpeed = 0, error = "" }, Cmd.none)
+init = ({ speed = 0
+        , requestedSpeed = 0
+        , startTime = 0.0
+        , currentTime = 0.0
+        , distance = 0.0
+        , error = "" }, Cmd.none)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -47,6 +56,29 @@ update msg model =
         DecreaseSpeed -> decreaseSpeed model
         SetSpeed speed -> changeSpeed model speed
         SetSpeedResponse result -> (updateSpeed model result, Cmd.none)
+        Tick t -> (updateTimeAndDistance model t, Cmd.none)
+
+
+updateTimeAndDistance : Model -> Time.Time -> Model
+updateTimeAndDistance model t =
+    -- This is a rough approximation because the time and speed are both inaccurate.
+    -- Speed would have to come from the treadmill, as it takes several seconds for
+    -- large changes in speed to occur.
+    let
+        timeStep =
+            if model.startTime > 0 then
+                Time.inHours (t - model.currentTime)
+            else
+                0.0
+    in
+    if model.speed > 0 then
+        { model
+            | currentTime = t
+            , startTime = if model.startTime == 0 then t else model.startTime
+            , distance = model.distance + (toFloat model.speed) * speed_increment * timeStep
+        }
+    else
+        model
 
 
 increaseSpeed : Model -> (Model, Cmd Msg)
@@ -75,8 +107,28 @@ decreaseSpeed model =
 
 updateSpeed : Model -> Result Http.Error String -> Model
 updateSpeed model result =
+    let
+        starting = model.speed == 0 && model.requestedSpeed /= 0
+        stoping = model.speed /= 0 && model.requestedSpeed == 0
+
+        updateModel =
+            if starting then
+                -- Have to check response...
+                { model
+                    | speed = model.requestedSpeed
+                    , startTime = 0.0
+                    , currentTime = 0.0
+                    , distance = 0.0
+                    , error = ""
+                }
+            else
+                { model
+                    | speed = model.requestedSpeed
+                    , error = ""
+                }
+    in
     case result of
-        Ok response -> { model | speed = model.requestedSpeed, error = "" } -- Have to check response...
+        Ok response -> updateModel
         Err error -> { model | requestedSpeed = model.speed, error = stringifyError error }
 
 
@@ -114,6 +166,31 @@ postSpeedChange requestedSpeed =
         Http.send (\r -> SetSpeedResponse r) req
 
 
+formatTime : Time.Time -> String
+formatTime t =
+    let
+        hours = floor(Time.inHours t)
+        minutes = floor(Time.inMinutes (t - (toFloat hours) * Time.hour))
+        seconds = floor(Time.inSeconds (t - (toFloat hours) * Time.hour - (toFloat minutes) * Time.minute))
+    in
+        (toString hours) ++ ":" ++ (toString minutes) ++ ":" ++ (toString seconds)
+
+
+formatDistance : Float -> String
+formatDistance d =
+    let
+        meters_thousands = floor(d)
+        meters_hundreds = floor((d * 10 - (toFloat meters_thousands)))
+        meters_tens = floor((d * 100 - (toFloat meters_hundreds)))
+        meters_ones = floor((d * 1000 - (toFloat meters_tens)))
+    in
+        (toString meters_thousands) ++ "."
+            ++ (toString meters_hundreds)
+            ++ (toString meters_tens)
+            ++ (toString meters_ones)
+            ++ " km"
+
+
 view : Model -> Html Msg
 view model =
     div [ class "cpanel" ]
@@ -121,9 +198,9 @@ view model =
               [ div [ class "cpanel-readout-speed" ]
                     [ text ((toString ((toFloat model.speed) * speed_increment)) ++ " km/h" ) ] 
               , div [ class "cpanel-readout-time" ]
-                  [ text "00:00" ] 
+                  [ text (formatTime (model.currentTime - model.startTime)) ]
               , div [ class "cpanel-readout-distance" ]
-                  [ text "0.00 km" ] ]
+                  [ text (formatDistance model.distance) ] ]
         , div [ class "cpanel-buttons" ]
             [ button
                   [ class "cpanel-button"
