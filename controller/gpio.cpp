1edf8ae3476f118e78619c5810382cf261b74ba6
Matt Hughes <matt.c.hughes@gmail.com>
2019-10-14 13:09:40 -0600

    Add code to read a safety sense pin

--- controller/gpio.cpp
+++ controller/gpio.cpp
@@ -10,6 +10,7 @@
 
 #define PWM_PIN 18
 #define SPEED_SENSE_PIN 17
+#define SAFETY_SENSE_PIN 27
 
 // 19.2e6 Hz / CLOCK_DIVISOR / RANGE = 20 Hz
 #define PWM_CLOCK_DIVISOR 3840
@@ -21,29 +22,39 @@ static struct timespec startTime;
 
 extern "C" {
 
-long getMillisecondsSinceStart()
-{
-  struct timespec currentTime;
-  clock_gettime(CLOCK_MONOTONIC, &currentTime);
+  long getMillisecondsSinceStart()
+  {
+    struct timespec currentTime;
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
 
-  time_t elapsedSeconds = currentTime.tv_sec - startTime.tv_sec;
-  long elapsedNanoSeconds = currentTime.tv_nsec - startTime.tv_nsec;
-  
-  return lround(elapsedSeconds * 1.0e3) + lround(elapsedNanoSeconds / 1.0e6);
-}
+    time_t elapsedSeconds = currentTime.tv_sec - startTime.tv_sec;
+    long elapsedNanoSeconds = currentTime.tv_nsec - startTime.tv_nsec;
 
-void pulse(void)
-{
-  counter++;
+    return lround(elapsedSeconds * 1.0e3) + lround(elapsedNanoSeconds / 1.0e6);
+  }
 
-  long msSinceStart = getMillisecondsSinceStart();
-  if (msSinceStart > 1000)
+  void pulse(void)
+  {
+    counter++;
+
+    long msSinceStart = getMillisecondsSinceStart();
+    if (msSinceStart > 1000)
     {
       frequency = round(counter / (msSinceStart / 1.0e3));
       clock_gettime(CLOCK_MONOTONIC, &startTime);
       counter = 0;
     }
-}
+  }
+
+  void safety_removed(void)
+  {
+    std::cerr << "Safety key was removed." << std::endl;
+  }
+
+  void safety_inserted(void)
+  {
+    std::cerr << "Safety key was inserted." << std::endl;
+  }
 }
 
 Gpio::Gpio()
@@ -69,28 +80,35 @@ void Gpio::init()
 
   pinMode(SPEED_SENSE_PIN, INPUT);
   pullUpDnControl(SPEED_SENSE_PIN, PUD_UP);
-
   wiringPiISR(SPEED_SENSE_PIN, INT_EDGE_RISING, &pulse);
+
+  pinMode(SAFETY_SENSE_PIN, INPUT);
+  pullUpDnControl(SAFETY_SENSE_PIN, PUD_UP);
+  wiringPiISR(SAFETY_SENSE_PIN, INT_EDGE_FALLING, &safety_inserted);
+  wiringPiISR(SAFETY_SENSE_PIN, INT_EDGE_RISING, &safety_removed);
+
+  int safetyStatus = digitalRead(SAFETY_SENSE_PIN);
+  std::cout << "Current safety status: " << safetyStatus << std::endl;
 
   clock_gettime(CLOCK_MONOTONIC, &startTime);
 }
-  
+
 void Gpio::shutdown()
 {
   pwmWrite(PWM_PIN, 0);
   pinMode(PWM_PIN, INPUT);
   pullUpDnControl(SPEED_SENSE_PIN, PUD_OFF);
 }
-  
+
 void Gpio::writePwm(int dutyCyclePercent)
 {
   // TODO check that the key is inserted. If not switch everything off.
-  
+
   if (dutyCyclePercent < 0 || dutyCyclePercent > 100)
     {
       throw std::range_error("Duty cycle must be in the range 0 <= dutyCyclePercent <= 100.");
     }
-  
+
   pwmWrite(PWM_PIN, round(dutyCyclePercent * 2.55));
 }
 
