7ed4eba4a510a799f87e6404a32c50be344c398e
Matt Hughes <matt.c.hughes@gmail.com>
2022-01-08 11:32:00 -0700

    Add logging to a file

--- nordichack/src/main.rs
+++ nordichack/src/main.rs
@@ -1,5 +1,8 @@
 use std::thread;
 use crossbeam_channel::{unbounded, Sender, Receiver};
+use simplelog::*;
+use log::*;
+use std::fs::File;
 
 mod nhtui;
 mod treadmill;
@@ -9,17 +12,19 @@ use crate::rptreadmill::PiTreadmill;
 use crate::treadmill::FakeTreadmill;
 
 fn main() {
-    println!("Hello, world!");
+    CombinedLogger::init(vec![WriteLogger::new(LevelFilter::Debug, Config::default(), File::create("nordichack.log").unwrap())]);
 
     let (event_tx, event_rx): (Sender<treadmill::Event>, Receiver<treadmill::Event>) = unbounded::<treadmill::Event>();
     let (command_tx, command_rx): (Sender<treadmill::Command>, Receiver<treadmill::Command>) = unbounded::<treadmill::Command>();
 
+    debug!("Starting UI thread...");
     let ui_thread = thread::spawn(move || {
         if let Err(err) = nhtui::tui(command_tx, event_rx) {
             println!("Failed to start UI thread: {}", err);
         };
     });
 
+    debug!("Starting treadmill...");
     #[cfg(all(feature = "real_treadmill", feature = "fake_treadmill"))]
     compile_error!("Only one of real_treadmill and fake_treadmill may be used.");
 
@@ -44,8 +49,11 @@ fn main() {
         }
     }
 
+    debug!("Waiting for UI thread to exit...");
     if let Err(err) = ui_thread.join() {
         println!("Failed to join UI thread: {:?}", err);
     }
+
+    debug!("All done.");
 }
 
